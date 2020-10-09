bfd52be07d38b2b701de476a0d9c9f1553d49d03
davidsmorton <davidsmorton@gmail.com>
2020-10-09 15:05:38 +0000

    Complete before refactoring

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,21 +7,21 @@
 // database....
 ////Helper Functions
 const escape =  function(str) {
-  let div = document.createElement('div');
+  const div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
-}
+};
 
-const hideError = function () {
+const hideError = function() {
 
   $("#errorContainer").slideUp(150);
   $("#errorText").html("");
-}
-  const showError = function (message) {
+};
+const showError = function(message) {
 
-    $("#errorText").html(message);
-    $("#errorContainer").slideDown(150); 
- }
+  $("#errorText").html(message);
+  $("#errorContainer").slideDown(150);
+};
 
 // JQUERY USED
 $(document).ready(function() {
@@ -29,10 +29,12 @@ $(document).ready(function() {
   postTweetsToServer();
   hideError();
 
-  $("#tweet-text").keypress(function (e) {
-    if(e.which == 13 && !e.shiftKey) {        
-        $(this).closest("form").submit();
-        e.preventDefault();
+  //Allows User to enter text with "enter key"
+  // reference stackoverflow
+  $("#tweet-text").keypress(function(e) {
+    if (e.which == 13 && !e.shiftKey) {
+      $(this).closest("form").submit();
+      e.preventDefault();
 
     }
   });
@@ -40,102 +42,96 @@ $(document).ready(function() {
 
 // Function that takes formatted tweets and saves them in the DOM
 const renderTweets = function(tweets) {
-  let tweetsContainer = $('#tweets-container')
+  const tweetsContainer = $('#tweets-container');
   tweetsContainer.empty();
   tweets.forEach(tweet => {
-    let formattedTweet = tweetFormatter(tweet)
+    const formattedTweet = tweetFormatter(tweet);
     tweetsContainer.append(formattedTweet); //update the DOM here
-  } )
-
-}
-
-// Function that formats (CSS & HTML) tweets from server 
-const tweetFormatter = function(tweet) {
-  // check out object destructing...
-  let date = new Date(tweet.created_at) 
-   return (
-     $(
-    `<article class="tweet">
-      <header>
-        <div class="picName">
-          <img class="profilePic" src="${escape(tweet.user.avatars)}">   
-          <p class="userName">${escape(tweet.user.name)}</p> 
-        </div>
-        <p class="handle">${escape(tweet.user.handle)}</p>
-      </header>
-      <p class="actualTweetText">${escape(tweet.content.text)}</p> 
-      <footer>
-        <p class="tweetDate">${escape(date.toLocaleString())}</p>  
-        <div class="icons">
-          <img src="/images/flag.jpg">
-          <img src="/images/retweet.svg">
-          <img src="/images/heart.png">
-        </div>
-      </footer>
-    </article>`
-      )
-) 
+  });
 };
 
-//AJAX Requests (jQuery response)
+// Function that formats (CSS & HTML) tweets from server
+const tweetFormatter = function(tweet) {
+  // check out object destructing...
+  const date = new Date(tweet.created_at);
+  return $(`
+      <article class="tweet">
+        <header>
+          <div class="picName">
+            <img class="profilePic" src="${escape(tweet.user.avatars)}">   
+            <p class="userName">${escape(tweet.user.name)}</p> 
+          </div>
+          <p class="handle">${escape(tweet.user.handle)}</p>
+        </header>
+        <p class="actualTweetText">${escape(tweet.content.text)}</p> 
+        <footer>
+          <p class="tweetDate">${escape(date.toLocaleString())}</p>  
+          <div class="icons">
+            <img src="/images/flag.jpg">
+            <img src="/images/retweet.svg">
+            <img src="/images/heart.png">
+          </div>
+        </footer>
+      </article>
+    `);
+};
 
-// POST // 
+//AJAX Requests (jQuery)
 
-const postTweetsToServer = function () { 
-  const form = $('.new-tweet .subTweet'); 
-  form.on('submit', function (event) {
-    event.preventDefault(); 
-    const errorNoText = "Please enter text to tweet your thoughts"
-    const errorTooLongTweet = "Please reduce the number of characters in your tweet to 140."
+// POST //
 
-    //select error message with conditional
-    //assign CSS style
-    //Use jQuery to hide or toggle on truthy value for first two conditions
+const postTweetsToServer = function() {
+  const form = $('.new-tweet .subTweet');
+  form.on('submit', function(event) {
+    event.preventDefault();
+    const errorNoText = "Please enter text to tweet your thoughts";
+    const errorTooLongTweet = "Please reduce the number of characters in your tweet to 140.";
 
-    
-    
-    
-    if (!$('#tweet-text').val().length) {
+
+    if ($('#tweet-text').val().length <= 0)  {
       hideError();
-      //
       showError(errorNoText);
-     } else if ( $('#tweet-text').val().length > 140) {
-      // go back to hidden then produce below
+    } else if ($('#tweet-text').val().length > 140) {
       hideError();
-      //
       showError(errorTooLongTweet);
-      } else {
-        hideError();
-            console.log('Button clicked, performing ajax call...');
-            // Sending data changing it to HTTP string
-            $.ajax('/tweets', { method: 'POST', data: $(this).serialize()})
-              .then(function (response) { 
-                console.log('Success: ', response);
-                $('#tweet-text').val("");
-                loadTweets();
-            })
-              .catch(function (err) {
-                console.log('Error:', err)
-              });
-              
-            } 
-          });
-}
-// GET // 
-const loadTweets = function () {
+    } else {
+      hideError();
+      console.log('Button clicked, performing ajax call...');
+      // Sending data changing it to HTTP string
+      $.ajax('/tweets', {
+        method: 'POST',
+        data: $(this).serialize()
+      })
+        .then(function(response) {
+          console.log('Success: ', response);
+          $('#tweet-text').val("");
+          $('.counter').val(140);
+          loadTweets();
+        })
+        .catch(function(err) {
+          console.log('Error:', err);
+        });
+            
+    }
+  });
+};
+// GET //
+const loadTweets = function() {
 
-  // Getting data in json 
+  // Getting data in json
 
-  $.ajax('/tweets', { method: 'GET', data: '#tweets-container'})
-
-    .then(function (response) { 
-    console.log('Success: ', response);
-    renderTweets(response.reverse());
+  $.ajax('/tweets', {
+    method: 'GET',
+    data: '#tweets-container'
   })
-    .catch(function (err) {
-    console.log('Error:', err)
-});
-  };
+    .then(function(response) {
+      console.log('Success: ', response);
+      renderTweets(response.reverse());
+    })
+    .catch(function(err) {
+      console.log('Error:', err);
+    });
+};
 
 
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,17 +3,17 @@
 
 $(document).ready(function() {
  
- $('#tweet-text').on('keydown', function (event) { 
-  const keyStrokes = $(this).val().length
-  const newCounter = 140 - keyStrokes
-  const counter = $(this).siblings('div').children('.counter')
-  counter.val(newCounter)
-   if (newCounter < 0) {
-     counter.addClass('red')
-   } else {
-     counter.removeClass('red');
-   }
- });
+  $('#tweet-text').on('keydown', function(event) {
+    const keyStrokes = $(this).val().length;
+    const newCounter = 140 - keyStrokes;
+    const counter = $(this).siblings('div').children('.counter');
+    counter.val(newCounter);
+    if (newCounter < 0) {
+      counter.addClass('red');
+    } else {
+      counter.removeClass('red');
+    }
+  });
   
 });
 
